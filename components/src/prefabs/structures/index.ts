77a4baa783900e03eda0a9d16f75daafdec75f82
Sam <39275315+degrootsam@users.noreply.github.com>
2025-11-24 12:38:11 +0100

    feat: added onRender component

---
+++ components/src/prefabs/structures/index.ts
@@ -0,0 +1,7 @@
+// Import all prefabs here
+import { OnRender } from './OnRender';
+
+// Import all prefab options here
+import { onRenderOptions } from './OnRender/options';
+
+export { OnRender, onRenderOptions };
